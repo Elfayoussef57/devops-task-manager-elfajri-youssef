03e0b0041ccee4d7663f540ee5ea6325e956dc83
Elfayoussef57 <pr.elfajriyoussef.57@gmail.com>
2026-02-18 12:15:07 +0000

    Change-default-tasks

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let tasks = [
 ];
 
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running (Lab 1)" });
+  res.json({ message: "feature default task test" });
 });
 
 app.get('/tasks', (req, res) => {
