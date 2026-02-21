4bb7880c1fbcf201a60e2c8b78d1ebe17cbf9d09
Elfayoussef57 <pr.elfajriyoussef.57@gmail.com>
2026-02-21 22:04:24 +0000

    Update welcome message on main branch

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let tasks = [
 ];
 
 app.get('/', (req, res) => {
-  res.json({ message: "feature default task test" });
+  res.json({ message: "Welcome from MAIN branch" });
 });
 
 app.get('/tasks', (req, res) => {
